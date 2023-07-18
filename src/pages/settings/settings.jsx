c90d8243af28d220c5a1d4b8e510516a17b25efe
Inish-Bashyal <inish.bashyal99@gmail.com>
2023-07-18 23:08:50 +0545

    Some css works

--- src/pages/settings/settings.jsx
+++ src/pages/settings/settings.jsx
@@ -1,38 +1,34 @@
+import React from 'react';
 import "./settings.css";
 import pic from "/Users/inishbashyal/Documents/FitBit_web/frontend/fitbit/src/assets/images/sidebar.jpeg";
 
- 
 export default function Setting() {
   return (
     <div className="settings">
       <div className="settingsWrapper">
-          <div className="settingsTitle">
-            <span className="settingsUpdateTitle">Update your account</span>
-            <span className="settingsDeleteTitle">Delete your account</span>
-          </div>
-          <form className="settingsForm">
-            <label>Profile Picture</label>
-            <div className="settingsPP">
-                <img 
-                src={pic}
-                alt=""
-                />
-              <label htmlFor="fileInput">
+        <form className="settingsForm">
+          <label>Profile Picture</label>
+          <div className="settingsPP">
+            <img src={pic} alt="" />
+            <label htmlFor="fileInput">
               <i className="settingsPPIcon fa-regular fa-user"></i>
-              </label>
-              <input type="file" id="fileInput" style={{display:"none"}} />
-            </div>
+            </label>
+            <input type="file" id="fileInput" style={{ display: "none" }} />
+          </div>
 
-            <label>Username</label>
-            <input type="text" placeholder="Username"/>
+          <label>Username</label>
+          <input type="text" placeholder="Username" />
 
-            <label>Email</label>
-            <input type="text" placeholder="email@gmail.com"/>
+          <label>Email</label>
+          <input type="text" placeholder="email@gmail.com" />
 
-            <label>Password</label>
-            <input type="password" />
+          <label>Password</label>
+          <input type="password" />
+          <div className="buttonContainer">
             <button className="settingsSubmit">Update</button>
-          </form>
+            <button className="settingsDelete">Delete Account</button>
+          </div>
+        </form>
       </div>
     </div>
   )
